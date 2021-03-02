ac6e967be7ce162490904773ba8b0053228d8d2e
Akhil Menon <akh180599@gmail.com>
2021-03-02 15:29:45 +0530

    Share Functionality

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -17,6 +17,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import android.os.AsyncTask
 import android.provider.MediaStore
 import android.widget.Toast
@@ -195,8 +196,16 @@ class MainActivity : AppCompatActivity() {
             super.onPostExecute(result)
             if(!result!!.isEmpty()){
                 Toast.makeText(this@MainActivity, "File saved: $result", Toast.LENGTH_SHORT).show()
-            }
 
+            }
+            MediaScannerConnection.scanFile(this@MainActivity, arrayOf(result),null){
+                path, uri -> val shareIntent = Intent()
+                shareIntent.action = Intent.ACTION_SEND
+                shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+                shareIntent.type = "image/png"
+
+                startActivity(Intent.createChooser(shareIntent, "Share"))
+            }
         }
 
     }
